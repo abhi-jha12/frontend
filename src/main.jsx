ce65e2d0c8da2d991d877cf3920dada11f6c4eaf
abhi-jha12 <jhaabhishek887@gmail.com>
2024-01-10 14:34:40 +0530

    main edit

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import App from './App.jsx'
 import './index.css'
 import { PriceProvider } from "./PriceContext";
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
     <PriceProvider>
     <App />
   </PriceProvider>
   </React.StrictMode>,
+  document.getElementById('root')
 )
