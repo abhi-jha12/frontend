f29f8c62481851b1fccf2e68c21e919632fb1052
abhi-jha12 <jhaabhishek887@gmail.com>
2024-01-11 20:55:09 +0530

    edits

--- src/components/cards/ImageCard.jsx
+++ src/components/cards/ImageCard.jsx
@@ -1,18 +1,37 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import AddButton from "../button/AddButton";
 import { Link } from "react-router-dom";
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+import firebaseConfig from "../../firebaseConfig";
+
+initializeApp(firebaseConfig);
+const db = getFirestore();
 
 function ImageCard({ pid, btn_name }) {
-  const fetchedUrl = "https://picsum.photos/seed/picsum/400/300";
+  const [fetchedUrl, setFetchedUrl] = useState("");
+
+  useEffect(() => {
+    const fetchproducturl = async () => {
+      const urlCollection = collection(db, "topsearch");
+      const snapshot = await getDocs(urlCollection);
+      const firstDoc = snapshot.docs[0];
+      const url = firstDoc.data().url;
+      setFetchedUrl(url);
+    };
+
+    fetchproducturl();
+  }, []);
+
   return (
     <div className="flex flex-col justify-between">
-      <Link to='/product1'>
-      <div
-        style={{ "--image-url": `url(${fetchedUrl})` }}
-        className=" bg-[image:var(--image-url)] max-w-[400px] h-[300px] w-full m-auto py-16 px-4 relative group rounded-2xl"
-      ></div>
+      <Link to="/product1">
+        <div
+          style={{ "--image-url": `url(${fetchedUrl})` }}
+          className=" bg-[image:var(--image-url)] max-w-[400px] h-[300px] w-full m-auto py-16 px-4 relative group rounded-2xl"
+        ></div>
       </Link>
-      
+
       <div className="flex flex-col font-overpass m-auto justify-center">
         <AddButton
           name={btn_name ?? "Empty button"}
---
+++ src/firebaseConfig.jsx
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA3y9MjaJ0-POtRggemrnlk_yD_yNgIznM",
+  authDomain: "gbbfull-2f55c.firebaseapp.com",
+  projectId: "gbbfull-2f55c",
+  storageBucket: "gbbfull-2f55c.appspot.com",
+  messagingSenderId: "1073633251413",
+  appId: "1:1073633251413:web:5f4680ad92cdc8a05c44dc",
+  measurementId: "G-PWXEYD2587"
+};
+export default firebaseConfig;
