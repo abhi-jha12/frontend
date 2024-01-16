3aed0088f62abb06f123eee160017eedbfcac7a2
abhi-jha12 <138577061+abhi-jha12@users.noreply.github.com>
2024-01-16 18:32:44 +0530

    Update Wishlist.jsx

--- src/pages/wishlist/Wishlist.jsx
+++ src/pages/wishlist/Wishlist.jsx
@@ -14,7 +14,7 @@ function Wishlist()  {
 
   useEffect(() => {
     const fetchproducts = async () => {
-      const productsCollection = collection(db, "Product");
+      const productsCollection = collection(db, "Products");
       const snapshot = await getDocs(productsCollection);
       const fetchedItems = snapshot.docs.map((doc) => ({
         id: doc.id,
@@ -33,7 +33,6 @@ function Wishlist()  {
     <div className='flex flex-wrap md:flex-row relative gap-4 justify-center'>
     {productsData.map((product) => (
         <ProductCard
-          key={product.id}
           pid={product.id}
           btn_name="MOVE TO CART"
           img={product.img}
