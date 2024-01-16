133dc3e21b40b02f1ae6eb5640fe6d843c98f262
abhi-jha12 <138577061+abhi-jha12@users.noreply.github.com>
2024-01-16 18:11:43 +0530

    Update ProductCard.jsx

--- src/components/cards/ProductCard.jsx
+++ src/components/cards/ProductCard.jsx
@@ -3,22 +3,21 @@ import AddButton from "../button/AddButton";
 import { PriceContext } from "../../PriceContext";
 import { Link } from "react-router-dom";
 
-function ProductCard({ pid, btn_name }) {
-  const { selectedSize, prices } = useContext(PriceContext);
-  const selectedPrice = prices[selectedSize];
-  const fetchedUrl = "https://picsum.photos/seed/picsum/220/250";
+function ProductCard({ pid, btn_name, img, price }) {
+  const selectedPrice = useContext(PriceContext);
+  
   return (
     <>
       <div className="flex flex-col justify-center ">
-        <Link to="/product1">
+        <Link to={`/product/${pid}`}>
           <div
-            style={{ "--image-url": `url(${fetchedUrl})` }}
+            style={{ backgroundImage: `url(${img})` }}
             className=" bg-[image:var(--image-url)] max-w-[220px] h-[250px] w-full m-auto py-16 px-4 relative group rounded-2xl"
           ></div>
         </Link>
         <div className="flex flex-col font-overpass m-auto justify-center">
           <h3 className="text-center font-bold mt-6 -mb-10  text-lg ">
-            &#8377;{[selectedPrice ?? 9999]}
+            &#8377;{price ?? selectedPrice ?? 9999}
           </h3>
 
           <AddButton
