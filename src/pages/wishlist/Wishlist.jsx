6ad5933fe7682832e7c0bb48096ca289e9dc0246
abhi-jha12 <138577061+abhi-jha12@users.noreply.github.com>
2024-01-16 18:09:11 +0530

    Update Wishlist.jsx

--- src/pages/wishlist/Wishlist.jsx
+++ src/pages/wishlist/Wishlist.jsx
@@ -1,27 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 import Header from "../../components/nav/Header";
 import Footer from "../../components/footer/Footer";
 import ProductCard from '../../components/cards/ProductCard';
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+import firebaseConfig from "../../firebaseConfig";
+
+initializeApp(firebaseConfig);
+const db = getFirestore();
 
 function Wishlist()  {
+  const [productsData, setProductsData] = useState([]);
+
+  useEffect(() => {
+    const fetchproducts = async () => {
+      const productsCollection = collection(db, "Product");
+      const snapshot = await getDocs(productsCollection);
+      const fetchedItems = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data()
+      }));
+      setProductsData(fetchedItems);
+    };
+
+    fetchproducts();
+  }, []);
+  
   return (
     <>
     <Header />
     <h1 className="text-center font-bold font-overpass text-4xl my-7 ">WISHLIST ITEMS</h1>
     <div className='flex flex-wrap md:flex-row relative gap-4 justify-center'>
-    <ProductCard pid='1' btn_name="MOVE TO CART" />
-    <ProductCard pid='2' btn_name="MOVE TO CART" />
-    <ProductCard pid='3' btn_name="MOVE TO CART" />
-    <ProductCard pid='3' btn_name="MOVE TO CART" />
-    <ProductCard pid='4' btn_name="MOVE TO CART" />
-    <ProductCard pid='5' btn_name="MOVE TO CART" />
-    <ProductCard pid='6' btn_name="MOVE TO CART" />
-    <ProductCard pid='7' btn_name="MOVE TO CART" />
-    <ProductCard pid='8' btn_name="MOVE TO CART" />
-    <ProductCard pid='9' btn_name="MOVE TO CART" />
-    <ProductCard pid='10' btn_name="MOVE TO CART" />
-    <ProductCard pid='11' btn_name="MOVE TO CART" />
-    </div>
+    {productsData.map((product) => (
+        <ProductCard
+          key={product.id}
+          pid={product.id}
+          btn_name="MOVE TO CART"
+          img={product.img}
+          price={product.price}
+        />
+      ))}
+      </div>
     
     <Footer />
 
