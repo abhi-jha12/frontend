86fdd7835b764f4ebd32ec121d92aaf0774ea966
abhi-jha12 <jhaabhishek887@gmail.com>
2024-01-16 21:57:22 +0530

    edits

--- src/components/hero/Poster.jsx
+++ src/components/hero/Poster.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
+import React,{useContext} from 'react';
 import Button from '../button/Button';
 
-function Poster() {
-  const fetchedUrl = "https://picsum.photos/seed/picsum/1100/300?grayscale";
+function Poster({btnname, posterimg, redirect }) {
   return (
-    <div style={{'--image-url': `url(${fetchedUrl})`}} className=" bg-[image:var(--image-url)] max-w-[1100px] h-[300px] w-full m-auto py-16 px-6 relative group rounded-2xl">
+    <div style={{ backgroundImage: `url(${posterimg})` }} className=" bg-[image:var(--image-url)] max-w-[1100px] h-[300px] w-full m-auto py-16 px-6 relative group rounded-2xl">
         <div className="flex justify-end  mt-10 py-20 rounded-lg max-md:mt-10 max-md:pr-5" >
-        <Button name="Shop Now" location='/shop' textSize='sm' />
+        <Button name={btnname ?? "Empty button"} location={redirect ?? '/shop'} textSize='sm' />
         </div>
     </div>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,36 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
 import Header from "../components/nav/Header";
 import ImageCarousel from "../components/hero/ImageCarousel";
 import Button from "../components/button/Button";
 import Poster from "../components/hero/Poster";
 import ImageCard from "../components/cards/ImageCard";
 import Footer from "../components/footer/Footer";
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+import firebaseConfig from "../firebaseConfig";
+
+initializeApp(firebaseConfig);
+const db = getFirestore();
 
 
 function Home() {
+
+  const [postersData, setPostersData] = useState([]);
+
+  useEffect(() => {
+    const fetchposters = async () => {
+      const postersCollection = collection(db, "poster");
+      const snapshot = await getDocs(postersCollection);
+      const fetchedItems = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data()
+      }));
+      setPostersData(fetchedItems);
+    };
+
+    fetchposters();
+  }, []);
+
   return (
     <>
       <Header />
@@ -23,8 +46,13 @@ function Home() {
         <Button name=" Get Your Size" textSize="text-2xl" />
       </div>
       <div className="flex flex-col gap-20 mx-5">
-        <Poster />
-        <Poster />
+      {postersData.map((product) => (
+        <Poster
+          btnname={product.btnname}
+          posterimg={product.posterimg}
+          redirect={product.redirect}
+        />
+      ))}
       </div>
       <div className="container mx-auto flex items-center justify-center flex-1 text-black text-center text-4xl font-bold self-center max-w-[1061px] mt-20 md:shrink  max-md:mt-5">
         <h1 className="text-center font-bold font-overpass text-4xl my-7 tracking-widest ">TOP SEARCHES</h1>
