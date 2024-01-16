e45ca1f668c1ca08db1ee1c471820746d86c29e7
abhi-jha12 <jhaabhishek887@gmail.com>
2024-01-16 21:08:15 +0530

    debugging

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,26 +1,42 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import Header from "../../components/nav/Header";
 import Footer from "../../components/footer/Footer";
 import ProductCard from "../../components/cards/ProductCard";
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+import firebaseConfig from "../../firebaseConfig";
+
+initializeApp(firebaseConfig);
+const db = getFirestore();
 
 function Shop() {
+  const [productsData, setProductsData] = useState([]);
+  useEffect(() => {
+    const fetchproducts = async () => {
+      const productsCollection = collection(db, "Products");
+      const snapshot = await getDocs(productsCollection);
+      const fetchedItems = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data()
+      }));
+      setProductsData(fetchedItems);
+    };
+
+    fetchproducts();
+  }, []);
   return (
     <div>
       <Header />
       <h1 className="text-center font-overpass font-bold text-4xl my-7 ">ALL PRODUCTS</h1>
       <div className="flex flex-wrap md:flex-row relative gap-4 justify-center">
-        <ProductCard pid="1" btn_name=" ADD TO CART" />
-        <ProductCard pid="2" btn_name=" ADD TO CART" />
-        <ProductCard pid="3" btn_name=" ADD TO CART" />
-        <ProductCard pid="3" btn_name=" ADD TO CART" />
-        <ProductCard pid="4" btn_name=" ADD TO CART" />
-        <ProductCard pid="5" btn_name=" ADD TO CART" />
-        <ProductCard pid="6" btn_name=" ADD TO CART" />
-        <ProductCard pid="7" btn_name=" ADD TO CART" />
-        <ProductCard pid="8" btn_name=" ADD TO CART" />
-        <ProductCard pid="9" btn_name=" ADD TO CART" />
-        <ProductCard pid="10" btn_name=" ADD TO CART" />
-        <ProductCard pid="11" btn_name=" ADD TO CART" />
+      {productsData.map((product) => (
+        <ProductCard
+          pid={product.id}
+          btn_name="MOVE TO CART"
+          img={product.img}
+          price={product.price}
+        />
+      ))}
       </div>
       <Footer />
     </div>
