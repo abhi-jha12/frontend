284f58a240cbf8013ca0b8eca11f2f6f76628913
abhi-jha12 <jhaabhishek887@gmail.com>
2024-01-20 20:40:11 +0530

    btn bug fixed

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -8,7 +8,7 @@ function Button(props) {
 
   return (
     <button
-      className={`font-overpass justify-center text-white hover:text-black font-bold bg-neutral-950 hover:bg-white self-center items-stretch mt-11 mb-9 px-11 py-3 rounded-lg border-2 border-black ${props.textSize || 'text-lg'}`}
+      className={`font-overpass justify-center text-white hover:text-black font-bold bg-neutral-950 hover:bg-white self-center items-stretch mt-11 mb-9 px-11 py-3 rounded-lg border-2 border-black whitespace-pre ${props.textSize || 'text-lg'}`}
       onClick={() => navigate(btn_navigate)}
     >
       {btn_text}
