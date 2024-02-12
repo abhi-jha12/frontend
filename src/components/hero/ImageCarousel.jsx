4d990b0007308a371a9b946157348e22ca6f163c
abhi-jha12 <jhaabhishek887@gmail.com>
2024-02-13 00:34:05 +0530

    changed carousel

--- src/components/hero/ImageCarousel.jsx
+++ src/components/hero/ImageCarousel.jsx
@@ -13,6 +13,7 @@ const db = getFirestore();
 function ImageCarousel() {
   const [slides, setSlides] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
 
   useEffect(() => {
     const fetchImages = async () => {
@@ -21,7 +22,9 @@ function ImageCarousel() {
         const docSnap = await getDoc(docRef);
 
         if (docSnap.exists()) {
-          const imageUrls = docSnap.data().carimg;
+          // Choose the field based on screen size
+          const fieldName = isMobile ? "mobilecarimg" : "carimg";
+          const imageUrls = docSnap.data()[fieldName];
           const slidesData = imageUrls.map((url) => ({ url }));
           setSlides(slidesData);
         } else {
@@ -33,6 +36,14 @@ function ImageCarousel() {
     };
 
     fetchImages();
+  }, [isMobile]); 
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
   }, []);
 
   const prevSlide = () => {
@@ -52,31 +63,25 @@ function ImageCarousel() {
   };
 
   return (
-    <div className="w-[400px] h-[500px] md:w-[1300px] md:h-[580px] md:ml-3 md:mx-5 py-20">
+    <div className="w-[500px] h-[400px] md:w-[1300px] md:h-[580px] -ml-12 md:ml-3 md:mx-5 py-5 md:py-20">
       {slides.length > 0 && (
         <div
           style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
-          className="h-[350px] w-[330px] md:h-[450px] md:w-[1300px] m-auto rounded-2xl bg-center bg-cover duration-500"
+          className="h-[330px] w-[350px] md:h-[450px] md:w-[1300px] m-auto rounded-2xl bg-center bg-cover duration-500"
         ></div>
       )}
-      {/* Left Arrow */}
-
       <img
         src={backicon}
         onClick={prevSlide}
         alt="Previous"
-        className=" w-11 h-11 hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-transparent text-black cursor-pointer"
+        className="w-11 h-11 hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-transparent text-black cursor-pointer"
       />
-
-      {/* Right Arrow */}
-
       <img
         src={fronticon}
         onClick={nextSlide}
         alt="Next"
         className="w-11 h-11 hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-transparent text-black cursor-pointer"
       />
-
       <div className="flex top-4 justify-center -mt-10">
         {slides.map((_, slideIndex) => (
           <div
